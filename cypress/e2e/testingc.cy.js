ee6c7122bf3618c17f76aedd3f0a39bce40c7c13
Makko420 <laidinen.marco@gmail.com>
2023-08-05 19:11:12 +0300

    fix bug

--- cypress/e2e/testingc.cy.js
+++ cypress/e2e/testingc.cy.js
@@ -1,6 +1,6 @@
 describe('Pokedex', function() {
   it('front page can be opened', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3000')
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
@@ -8,7 +8,7 @@ describe('Pokedex', function() {
 
 describe('Pokedex', function() {
   it('navigation work yes ?', function() {
-    cy.visit('http://localhost:5000')
+    cy.visit('http://localhost:3000')
     cy.contains('rattata').click()
     cy.get('.pokemon-ability-name:first').contains('run away')
     cy.get('.pokemon-ability-name:last').contains('hustle')
